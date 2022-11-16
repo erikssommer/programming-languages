7445bc55032b64719130ca5fa6bba6d92b2de312
Erik Storås Sommer <erikssommer@gmail.com>
2022-11-16 19:20:02 +0100

    Added facts for the problem to be solved

---
+++ relational-and-contraint-programming/relational.pl
@@ -0,0 +1,11 @@
+:- use_module(library(clpfd)).
+
+% Defines the facts of our problem, that is, the list of cabins that will be used to test your implementation and the distances between them.
+
+distance(c1, c2, 10, 1). distance(c1, c3, 0, 0). distance(c1, c4, 7, 1).
+distance(c1, c5, 5, 1). distance(c2, c3, 4, 1). distance(c2, c4, 12, 1).
+distance(c2, c5, 20, 1). distance(c3, c4, 0, 0). distance(c3, c5, 0, 0).
+distance(c4, c5, 0, 0). distance(c2, c1, 10, 1). distance(c3, c1, 0, 0).
+distance(c4, c1, 7, 1). distance(c5, c1, 5, 1). distance(c3, c2, 4, 1).
+distance(c4, c2, 12, 1). distance(c5, c2, 20, 1). distance(c4, c3, 0, 0).
+distance(c5, c3, 0, 0). distance(c5, c4, 0, 0).
