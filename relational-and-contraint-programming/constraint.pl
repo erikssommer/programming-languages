b2416fe9347787307b668f45b90e012ae943a5cd
Erik Storås Sommer <erikssommer@gmail.com>
2022-11-16 19:15:56 +0100

    Constraint programming in prolog

---
+++ relational-and-contraint-programming/constraint.pl
@@ -0,0 +1,16 @@
+:- use_module(library(clpfd)). % Prolog library for Constraint Logic Programming.
+
+% A small prolog predicate that takes a list of coins as input.
+% The predicate accepts a payment sum and a list of coin(Count,Value,Limit).
+
+payment(0, []).
+payment(Amount, [coin(Count,Value,Limit)|Tail]) :-
+    Count in 0..Limit,
+    payment(Remaining, Tail),
+    Amount #= Count * Value + Remaining.
+
+% Testing the predicate
+payment(25, [coin(Ones,1,11),coin(Fives,5,4),coin(Tens,10,3),coin(Twenties,20,2)]).
+
+payment(25, [coin(Ones,1,11),coin(Fives,5,4),coin(Tens,10,3),coin(Twenties,20,2)]), 
+label([Ones, Fives, Tens, Twenties]).
