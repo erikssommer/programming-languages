0ee399c999977ef235853766e42d308b1213a65d
Erik Storås Sommer <erikssommer@gmail.com>
2022-11-17 15:34:11 +0100

    Commenting out queries

--- relational-and-contraint-prog/constraint.pl
+++ relational-and-contraint-prog/constraint.pl
@@ -12,7 +12,7 @@ payment(Amount, [coin(Count,Value,Limit)|Tail]) :-
     Amount #= Count * Value + Remaining.
 
 % Testing the predicate
-payment(25, [coin(Ones,1,11),coin(Fives,5,4),coin(Tens,10,3),coin(Twenties,20,2)]).
+% payment(25, [coin(Ones,1,11),coin(Fives,5,4),coin(Tens,10,3),coin(Twenties,20,2)]).
 
-payment(25, [coin(Ones,1,11),coin(Fives,5,4),coin(Tens,10,3),coin(Twenties,20,2)]), 
-label([Ones, Fives, Tens, Twenties]).
+% payment(25, [coin(Ones,1,11),coin(Fives,5,4),coin(Tens,10,3),coin(Twenties,20,2)]), 
+% label([Ones, Fives, Tens, Twenties]).
